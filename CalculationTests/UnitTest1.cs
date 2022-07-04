0474d816d63ec1a2ee732c9f4c750f935a6c28c2
Tabula <MrZhnex@ya.ru>
2022-07-04 04:30:14 +0300

    Add unit test for triangle

--- CalculationTests/UnitTest1.cs
+++ CalculationTests/UnitTest1.cs
@@ -15,5 +15,15 @@ namespace CalculationTests
             double result = circle.GetArea();
             Assert.Equal(expected, result);
         }
+
+        [Fact]
+        public void TriangleGetAreaTest()
+        {
+            var triangle = new Triangle(2, 6, 6);
+
+            double expected = 5.916079783099616;
+            double result = triangle.GetArea();
+            Assert.Equal(expected, result);
+        }
     }
 }
