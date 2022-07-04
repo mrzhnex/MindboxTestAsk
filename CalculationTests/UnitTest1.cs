3ad44b5b872ae95f075280f130464fee7ee9511b
Tabula <MrZhnex@ya.ru>
2022-07-04 04:27:10 +0300

    Add unit test for circle

---
+++ CalculationTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using MTALib.Core;
+using Xunit;
+
+namespace CalculationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CircleGetAreaTest()
+        {
+            var circle = new Circle(3.0);
+
+            double expected = 28.274333882308138; //ожидаемый результат был найден с помощью калькулятора на сайте https://allcalc.ru/node/18
+            //примечание: несколько сайтов до этого округляли значение до 4 или 5 символов после запятой
+            double result = circle.GetArea();
+            Assert.Equal(expected, result);
+        }
+    }
+}
